f4a6e29664ad778c820998ccdeef2cfa1bd61a71
Matheus Fidelis <msfidelis@outlook.com>
2018-01-21 20:59:48 -0200

    organization

--- config/server.js
+++ config/server.js
@@ -1,9 +1,9 @@
 'use strict';
 
-const express = require('express')
-const consign = require('consign')
-const bodyParser = require('body-parser')
-const expressValidator = require('express-validator')
+const express       = require('express')
+const consign       = require('consign')
+const bodyParser    = require('body-parser')
+const validator     = require('express-validator')
 
 module.exports = () => {
 
@@ -12,7 +12,7 @@ module.exports = () => {
     app.use(bodyParser.urlencoded({ extended: true }));
     app.use(bodyParser.json());
 
-    app.use(expressValidator());
+    app.use(validator());
 
     consign()
         .include('routes')
